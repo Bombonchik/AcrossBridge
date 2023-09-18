a766a0ca408a86b1f7ee1854e0a34e91993bd002
Artem <akartemka2@gmail.com>
2023-09-18 20:34:48 +0300

    donate-info

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from settings import CHAIN, AMOUNT_FROM, AMOUNT_TO, ALL_AMOUNT, KEEP_VALUE_FROM,
 
 
 def main():
+    print(f'\n{" " * 32}AUTHOR - https://t.me/CodeCashMafia{" " * 32}\n')
+    print(f'\n{" " * 32}donate - EVM 0x98F93AD29d4Fe210b9D33E910335C88C333db87A{" " * 32}\n')
+
     random.shuffle(ACCOUNTS)
     for index, private_key in enumerate(ACCOUNTS):
         across = Across(private_key, index)
@@ -18,6 +21,8 @@ def main():
             logger.error(f'[{across.address}] error occured - {e}')
             time.sleep(random.randint(30, 60))
 
+    print(f'\n{" " * 32}AUTHOR - https://t.me/CodeCashMafia{" " * 32}\n')
+    print(f'\n{" " * 32}donate - EVM 0x98F93AD29d4Fe210b9D33E910335C88C333db87A{" " * 32}\n')
 
 if __name__ == '__main__':
     main()
